52ca4182fe3a4f078bf9a99f8f5e18dbfbe0b94f
Impulse3e <122300035+Impulse3e@users.noreply.github.com>
2024-10-29 13:18:53 +0300

    Update client.py
    NOT USING NOW

--- client.py
+++ client.py
@@ -1,18 +1,19 @@
-import socketio
+#NOT USING NOW
+# import socketio
 
-sio = socketio.Client()
+# sio = socketio.Client()
 
-@sio.event
-def connect():
-    print('Connected to server')
+# @sio.event
+# def connect():
+#     print('Connected to server')
 
-@sio.event
-def audio_alert(data):
-    print('Audio alert:', data['message'])
+# @sio.event
+# def audio_alert(data):
+#     print('Audio alert:', data['message'])
 
-@sio.event
-def disconnect():
-    print('Disconnected from server')
+# @sio.event
+# def disconnect():
+#     print('Disconnected from server')
 
-sio.connect('http://localhost:5000')
-sio.wait()
+# sio.connect('http://localhost:5000')
+# sio.wait()
